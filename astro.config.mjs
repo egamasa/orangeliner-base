674e03db3457586f76ea2c5538ed1b245034633e
egamasa <40469431+egamasa@users.noreply.github.com>
2026-04-20 21:38:05 +0900

    Add site switching by SITE_ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+const sites = {
+  web: 'https://orangeliner.net',
+  blog: 'https://blog.orangeliner.net',
+};
+
+const siteId = process.env.SITE_ID || 'web';
+
 // https://astro.build/config
 export default defineConfig({
+  site: sites[siteId],
   vite: {
     plugins: [
       tailwindcss(),
